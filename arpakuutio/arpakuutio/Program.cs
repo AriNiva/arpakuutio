94eb81702debdc9cba992e2ff0aa0a29250f51d5
AriNiva <145963185+AriNiva@users.noreply.github.com>
2023-09-25 10:13:13 +0300

    Add files via upload

---
+++ arpakuutio/arpakuutio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arpakuutio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean lopeta = false;
+            Console.WriteLine("Tervetuloa random-generaattoriin!");
+            while (lopeta != true) 
+            {
+                Console.WriteLine("Haetaan seuraavat Random-luvut...");
+                randomLuvut();
+                Console.WriteLine("Haetaanko luvut uudelleen? K=kyllä/E=ei");
+                if (Console.ReadLine().ToUpper() == "E") lopeta = true;
+            }
+               
+        }
+
+        private static void randomLuvut()
+        {
+            int rLuku = 0, i = 0;
+            Random arpa = new Random();
+            //silmukka alkaa ja pyörii niin kauan, kuin laskurimuuttuja on alle 100
+            while (i < 100)
+            {
+                rLuku = arpa.Next(1, 3000);
+                Console.WriteLine("Silmukan kierroksella {1} randomluku on {0}", rLuku.ToString(), (i + 1).ToString());
+                //kasvata silmukkalaskurin arvoa yhdellä per kierros
+                i++;
+            }
+        }
+    }
+}
